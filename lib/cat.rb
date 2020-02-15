0114c5fea0451e3ec694b8b413e8a6e919cb501f
Daniel Sun <danielsun12345@gmail.com>
2020-02-15 19:28:15 +0000

    Done.

--- lib/cat.rb
+++ lib/cat.rb
@@ -1,3 +1,15 @@
 class Cat
-  # code goes here
+  attr_accessor :owner, :mood
+  attr_reader :name
+
+
+  def initialize(name, owner)
+    @name = name 
+    @owner = owner 
+  end 
+  
+  def mood(mood = "nervous")
+    mood  
+  end 
+  
 end
