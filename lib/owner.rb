5ef7b7933cb2e8ca318e95a8db81847a45d8e1fb
Daniel Sun <danielsun12345@gmail.com>
2020-02-16 21:58:42 +0000

    Done.

--- lib/owner.rb
+++ lib/owner.rb
@@ -60,7 +60,8 @@ class Owner
   def sell_pets
     self.dogs.each {|dog| dog.mood = "nervous"}
     self.cats.each {|cat| cat.mood = "nervous"}
-
+    self.dogs.each {|dog| dog.owner = nil}
+    self.cats.each {|cat| cat.owner = nil}
   end 
   
   
